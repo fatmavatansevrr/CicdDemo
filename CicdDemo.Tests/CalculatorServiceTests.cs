4ff1a1ca8dd35d7b7c668770ee5ae9735e2e2618
fatmavatansevrr <vatanseverfatma01@gmail.com>
2026-07-29 17:04:28 +0300

    tesfix

--- CicdDemo.Tests/CalculatorServiceTests.cs
+++ CicdDemo.Tests/CalculatorServiceTests.cs
@@ -25,7 +25,7 @@ public sealed class CalculatorServiceTests
     [TestMethod]
     public void Subtract_WhenTwoNumbersAreProvided_ReturnsDifference()
     {
-        var result = _service.Subtr);
+        var result = _service.Subtract(10, 4);
 
         Assert.AreNotSame(6, result);
     }
